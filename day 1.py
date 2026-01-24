25082bf50e66f76c849c0c3b8f04f58b2b6094fc
Sameer Vhatkar <vhatkarsameer0@gmail.com>
2026-01-25 00:25:20 +0530

    Day 2: File Handling and OS interaction

--- day 1.py
+++ day 1.py
@@ -1,3 +1,9 @@
+#printing and taking inputs
+# print("Hello World")
+
+# print(input("Enter your name: "))
+
+#if-else case
 # age = int(input("Enter you age: "))
 # if(age >= 18):
 #     print("Can Vote")
@@ -9,13 +15,15 @@
 #     print(i)
 
 #Functions
-
+#1.
 # def display(name):
 #     print("Hello ", name)
 
 # name = input("Enter your name: ")
 # display(name)
 
+
+#.2
 # def add(a, b):
 #     return a + b
 
@@ -32,6 +40,6 @@ def greet(name, birthYear):
     print("Hello ", name, " you are ", int(date.today().year) - birthYear, " years old")
     
 name = input("Enter your name: ")
-birthYear = int(input("Enter your Birth Year"))
+birthYear = int(input("Enter your Birth Year: "))
 greet(name, birthYear)
 
